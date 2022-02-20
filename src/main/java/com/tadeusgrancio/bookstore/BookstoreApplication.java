ea34b2694464baa568e53546d76195967f0a2202
tadeusgrancio <tadeusgrancio@gmail.com>
2022-02-20 15:57:50 -0300

    instancias automaticas

--- src/main/java/com/tadeusgrancio/bookstore/BookstoreApplication.java
+++ src/main/java/com/tadeusgrancio/bookstore/BookstoreApplication.java
@@ -1,13 +1,41 @@
 package com.tadeusgrancio.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.tadeusgrancio.bookstore.domain.Categoria;
+import com.tadeusgrancio.bookstore.domain.Livro;
+import com.tadeusgrancio.bookstore.repositories.CategoriaRepository;
+import com.tadeusgrancio.bookstore.repositories.LivroRepository;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Ficção", "Fantasia e histórias imaginativas");
+		Livro l1 = new Livro(null, "Dança dos Dragões", "RR Martin", "Loren ipsum", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+		
 	}
 
 }
